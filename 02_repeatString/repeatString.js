e352601313fb5e90f2725c13d35c776dcf821b86
Zaringar <zaringar@outlook.com>
2022-05-12 16:12:43 +0200

    2 exersices done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
-};
+let Ergebniss = ''
+const repeatString = function(Text, wiederholungen) {
+    Ergebniss = ''
+  if (wiederholungen >= 0) {
+    for (let i = 0; i < wiederholungen; i++){
+        Ergebniss = Text.concat(Ergebniss);
+      }
+  } else {
+    Ergebniss = 'ERROR';
+  }
+    return(Ergebniss)
+   };
+   repeatString('hey',10);
+   console.log(Ergebniss)
 
 // Do not edit below this line
 module.exports = repeatString;
