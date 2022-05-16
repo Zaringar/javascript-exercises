9809a0cac8f7e76aa43a8f99ce97f68bcd7ec829
Zaringar <zaringar@outlook.com>
2022-05-16 02:48:10 +0200

    test1 bestanden

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(Reihe, remover) {
+    Reihe.splice(remover - 1, 1);
+    return Reihe;
+  }
+  removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
