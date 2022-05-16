7feb3313a37f9f48860159331034abed588cd063
Zaringar <zaringar@outlook.com>
2022-05-16 04:07:46 +0200

    lösungsweg gefunden

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,10 @@
-const removeFromArray = function(Reihe, remover) {
-    Reihe.splice(remover - 1, 1);
-    return Reihe;
-  }
-  removeFromArray([1, 2, 3, 4], 3);
-
+const removeFromArray = function(arr, ...args) {
+    let a = arr;
+    let b = args;
+    // Arrow function (nimm a als This in funktion und beinhalte alle werte von der function die ungleich b sind)
+    let c = a.filter(d => !b.includes(d));
+    return c;
+    console.log(c);
+  };
 // Do not edit below this line
 module.exports = removeFromArray;
