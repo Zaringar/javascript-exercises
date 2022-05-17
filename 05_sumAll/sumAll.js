f65a39d3c390bf3b9e08d72890be3f4fd9414d6f
Zaringar <zaringar@outlook.com>
2022-05-17 03:32:46 +0200

    Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
-};
+Summe = 0
+    const sumAll = function(a, b) {
+        Summe = 0
+        if (a < 0 || b < 0 || typeof a != 'number' || typeof b != 'number') {
+        return "ERROR";
+      } else if (a < b) {
+    while (b + 1 != a) {
+        Summe = Summe + a
+        a++;
+      }} else if (a > b) {
+        while (a + 1 != b) {
+            Summe = Summe +  b
+            b++;
+          }
+      }
+      return Summe;
+    }
 
 // Do not edit below this line
 module.exports = sumAll;
