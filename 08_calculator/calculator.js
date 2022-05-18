66c5911164aa7458428c0279875e349d7db0a92c
Zaringar <zaringar@outlook.com>
2022-05-18 04:49:42 +0200

    08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(number,number2) {
+	summe = number + number2;
+  return summe;
 };
 
-const subtract = function() {
-	
+const subtract = function(number, number2) {
+	summe = number - number2;
+  return summe;
+};
+const sum = function(numbers) {
+  var x = numbers.reduce(function(prev,curr){
+    return curr + prev;
+  },0);
+    return x;
 };
 
-const sum = function() {
-	
+const multiply = function(numbers) {
+ let Produkt = numbers.reduce((a, b) => a * b);
+    return Produkt;
 };
 
-const multiply = function() {
-
+const power = function(number, exponent) {
+	var Produkt = Math.pow(number, exponent)
+  return Produkt;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1)
+    return 1;
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
