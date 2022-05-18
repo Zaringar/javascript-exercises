850c401e216bb3afc47cc9630a1873594223dba8
Zaringar <zaringar@outlook.com>
2022-05-18 02:27:14 +0200

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(TemperaturF) {
+  let Celsius = (TemperaturF -32)*5/9
+  let CelsiusR = Math.round(Celsius * 10) / 10
+  return CelsiusR;
 };
 
-const ctof = function() {
-
+const ctof = function(TemperaturC) {
+  let Fahrenheit = TemperaturC * 1.8 + 32
+  let FahrenheitR = Math.round(Fahrenheit * 10) / 10
+  return FahrenheitR;
 };
 
 // Do not edit below this line
