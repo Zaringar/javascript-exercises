138defbf1dca3e6bdf855b5d6fe1704862b457e0
Zaringar <zaringar@outlook.com>
2022-05-18 02:02:43 +0200

    06 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(schaltjahr) {
+let rest = (schaltjahr % 4)
+let century = (schaltjahr % 100)
+let vierhundert = (schaltjahr % 400)
+if (vierhundert === 0){
+    return true;
+}
+else if (century === 0){
+    return false;
+}
+else if (rest === 0){
+    return true;
+}
+else {
+    return false;
+}
 };
 
 // Do not edit below this line
