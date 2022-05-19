56986bf28b77187b1292ce59774e105ee79b53d9
Zaringar <zaringar@outlook.com>
2022-05-19 04:13:25 +0200

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+let punctuation = str.replace(/\W+/g,"").toLowerCase();
+    punctuation = punctuation.toLowerCase();
+let newstr = punctuation.split("").reverse().join("");
+if (newstr === punctuation){
+    return true;
+    console.log(`true ${newstr}`)
+}
+else {
+    return false;
+    console.log(`false ${newstr}`)
+}
 };
 
 // Do not edit below this line
