b652eac2541dcd9f01dc49ba9ea1588f8a1c810b
Zaringar <zaringar@outlook.com>
2022-05-21 06:44:37 +0200

    exercise 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(Zahl) {
+    if (Zahl < 0)
+       return "OOPS";
+       if (Zahl === 0)
+       return 0;
+   
+       let a = 0
+       let b = 1
+   
+       for (let i = 1; i < Zahl; i++){
+       let temp = b
+       
+       b = a + b
+       a = temp
+     }
+     return b
+   };
 
 // Do not edit below this line
 module.exports = fibonacci;
