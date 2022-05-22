9f5ee9e8511581eaa96d9b23c2e89ff9e8258d90
Zaringar <zaringar@outlook.com>
2022-05-22 04:43:59 +0200

    11 done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,19 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
+const getTheTitles = function() {
+    const titles = books.map((newbooks) => {
+        return newbooks.title;
+    })
+    return titles
 };
 
 // Do not edit below this line
